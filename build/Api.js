23dae8b6182ef5b1dabce4abcb6ec26bdce267dd
Yako <danielyaa5@gmail.com>
2017-08-16 13:33:46 -0700

    1.0.1

--- build/Api.js
+++ build/Api.js
@@ -50,68 +50,39 @@ var Api = function () {
 
   (0, _createClass3.default)(Api, [{
     key: 'query',
-    value: function (_query) {
-      function query(_x, _x2, _x3, _x4, _x5, _x6) {
-        return _query.apply(this, arguments);
-      }
-
-      query.toString = function () {
-        return _query.toString();
-      };
-
-      return query;
-    }(function (adultCount, maxPrice, solutions, origin, destination, date) {
-      var options = arguments.length > 6 && arguments[6] !== undefined ? arguments[6] : {};
-
-      var endPoint = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=' + this.apikey;
-
-      var data = {};
-      data.request.passengers.adultCount = adultCount;
-      data.request.maxPrice = maxPrice;
-      data.request.solutions = solutions;
-      data.request.slice[0].origin = origin;
-      data.request.slice[0].destination = destination;
-      data.request.slice[0].date = date;
-
-      return Api._queryPromise(endPoint, query, options);
-    })
-  }], [{
-    key: '_queryPromise',
     value: function () {
-      var _ref = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee(endPoint, data, options) {
-        var response, body, writePath, tripOptions;
+      var _ref = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee(adultCount, maxPrice, solutions, origin, destination, date) {
+        var options = arguments.length > 6 && arguments[6] !== undefined ? arguments[6] : {};
+        var endPoint, data, queryRequest, queryResponse, writePath;
         return _regenerator2.default.wrap(function _callee$(_context) {
           while (1) {
             switch (_context.prev = _context.next) {
               case 0:
-                _context.next = 2;
-                return request({ method: 'post', url: endPoint, body: data, json: true });
+                endPoint = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=' + this.apikey;
+                data = {};
 
-              case 2:
-                response = _context.sent;
-                body = response.body;
+                data.request.passengers.adultCount = adultCount;
+                data.request.maxPrice = maxPrice;
+                data.request.solutions = solutions;
+                data.request.slice[0].origin = origin;
+                data.request.slice[0].destination = destination;
+                data.request.slice[0].date = date;
+
+                queryRequest = { method: 'post', url: endPoint, body: data, json: true };
+                _context.next = 11;
+                return Api._queryPromise(queryRequest);
+
+              case 11:
+                queryResponse = _context.sent;
                 writePath = options.write;
 
                 if (writePath) {
-                  fs.writeFileSync(writePath, (0, _stringify2.default)({ request: data, response: response }), 'utf8');
+                  fs.writeFileSync(writePath, (0, _stringify2.default)({ request: queryRequest, response: queryResponse }), 'utf8');
                 }
 
-                if (!body.error) {
-                  _context.next = 8;
-                  break;
-                }
+                return _context.abrupt('return', queryResponse);
 
-                throw body.error;
-
-              case 8:
-                tripOptions = body.trips.tripOption;
-                return _context.abrupt('return', tripOptions.map(function (tripOption) {
-                  var airline = tripOption.slice[0].segment[0].flight.carrier;
-                  var price = tripOption.saleTotal;
-                  return { airline: airline, price: price };
-                }));
-
-              case 10:
+              case 15:
               case 'end':
                 return _context.stop();
             }
@@ -119,8 +90,53 @@ var Api = function () {
         }, _callee, this);
       }));
 
-      function _queryPromise(_x8, _x9, _x10) {
+      function query(_x, _x2, _x3, _x4, _x5, _x6) {
         return _ref.apply(this, arguments);
+      }
+
+      return query;
+    }()
+  }], [{
+    key: '_queryPromise',
+    value: function () {
+      var _ref2 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee2(queryRequest) {
+        var response, body, tripOptions;
+        return _regenerator2.default.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.next = 2;
+                return request(queryRequest);
+
+              case 2:
+                response = _context2.sent;
+                body = response.body;
+
+                if (!body.error) {
+                  _context2.next = 6;
+                  break;
+                }
+
+                throw body.error;
+
+              case 6:
+                tripOptions = body.trips.tripOption;
+                return _context2.abrupt('return', tripOptions.map(function (tripOption) {
+                  var airline = tripOption.slice[0].segment[0].flight.carrier;
+                  var price = tripOption.saleTotal;
+                  return { airline: airline, price: price };
+                }));
+
+              case 8:
+              case 'end':
+                return _context2.stop();
+            }
+          }
+        }, _callee2, this);
+      }));
+
+      function _queryPromise(_x8) {
+        return _ref2.apply(this, arguments);
       }
 
       return _queryPromise;
