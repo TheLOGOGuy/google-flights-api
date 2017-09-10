bcd51900a0224344f18d0a7da8a827f65f8c4126
danielyaa5 <danielyaa5@gmail.com>
2017-09-09 17:14:13 -0700

    1.1.3

--- build/Api.js
+++ build/Api.js
@@ -112,7 +112,7 @@ Api.prototype.query = function () {
             queryBody = Api._getQueryBody(_.defaultsDeep(q, defaultQ));
             queryRequest = { method: 'POST', url: this.url, body: queryBody, json: true };
             _context.next = 12;
-            return Api._queryPromise(queryRequest);
+            return Api._queryPromise(queryRequest, { simple: this.options.simple });
 
           case 12:
             queryResponse = _context.sent;
@@ -199,7 +199,7 @@ Api.prototype.rawQuery = function () {
           case 12:
             queryRequest = { method: 'POST', url: this.url, body: q, json: true };
             _context2.next = 15;
-            return Api._queryPromise(queryRequest);
+            return Api._queryPromise(queryRequest, { simple: this.options.simple });
 
           case 15:
             queryResponse = _context2.sent;
@@ -250,7 +250,9 @@ Api._saveQueryData = function (savePath, q, req, res) {
 
 /**
  * Returns a promise which resolves to the response body
- * @param {Object} queryRequest - The request object made to the QPX api
+ * @param {Object} queryRequest          - The request object made to the QPX api
+ * @param {Object} [options={}]          - Optional parameters
+ * @param {Object} [options.simple=true] - If true will return error on invalid status code
  * @returns {Promise} Promise with query response body
  * @throws Will throw an error if request-promise fails
  * @static
@@ -258,34 +260,36 @@ Api._saveQueryData = function (savePath, q, req, res) {
  * @private
  */
 Api._queryPromise = function () {
-  var _ref3 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(queryRequest) {
+  var _ref3 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(queryRequest, options) {
     var response;
     return _regenerator2.default.wrap(function _callee3$(_context3) {
       while (1) {
         switch (_context3.prev = _context3.next) {
           case 0:
-            _context3.prev = 0;
-            _context3.next = 3;
-            return request(queryRequest, { simple: this.options.simple });
+            options = _.defaultsDeep(options, { simple: true });
 
-          case 3:
+            _context3.prev = 1;
+            _context3.next = 4;
+            return request(queryRequest, { simple: options.simple });
+
+          case 4:
             response = _context3.sent;
             return _context3.abrupt('return', response);
 
-          case 7:
-            _context3.prev = 7;
-            _context3.t0 = _context3['catch'](0);
+          case 8:
+            _context3.prev = 8;
+            _context3.t0 = _context3['catch'](1);
             return _context3.abrupt('return', _context3.t0);
 
-          case 10:
+          case 11:
           case 'end':
             return _context3.stop();
         }
       }
-    }, _callee3, this, [[0, 7]]);
+    }, _callee3, this, [[1, 8]]);
   }));
 
-  return function (_x5) {
+  return function (_x5, _x6) {
     return _ref3.apply(this, arguments);
   };
 }();
